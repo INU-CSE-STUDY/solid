24e711e8ca84bfcd70cf1d108f4ef8144ea94dcd
JIN <o3ouuu@inu.ac.kr>
2026-01-09 19:21:56 +0900

    docs: 불필요한 주석 삭제

--- src/main/java/study/spring/solid/part2/adapter/out/SaveAdapter.java
+++ src/main/java/study/spring/solid/part2/adapter/out/SaveAdapter.java
@@ -16,8 +16,6 @@ public class SaveAdapter implements SavePort {
     @Override
     public void save(Data data) {
         log.info("트랜잭션 내 데이터 저장 시작 - ID: {}, 내용: {}", data.getId(), data.getContent());
-        // TODO: 실제 저장 로직 (메모리, 파일 등)
-        // 저장 실패 시 예외 발생하여 트랜잭션 롤백
         log.info("트랜잭션 내 데이터 저장 완료 - ID: {}", data.getId());
     }
 }
